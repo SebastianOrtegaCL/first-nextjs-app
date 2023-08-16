b23d90ada4b7a474b0cd1c6d421ad77c0019c1ca
sebastian ortega <sebastian.ortega.fernandezz@gmail.com>
2023-08-16 01:11:27 -0400

    navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
+import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <h1> My App </h1>
+      <Navbar />
       <Component {...pageProps}/>
 
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -16,7 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      
       <main className={`${styles.main} ${inter.className}`}>
         <Link href='/about' className = {styles.title}>
           Go to About
